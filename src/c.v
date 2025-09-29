73850d0959bacf0f3aa437dbbeb34368fa7fbcfc
KingBes <48203677+KingBes@users.noreply.github.com>
2025-09-29 12:31:38 +0800

    Update c.v

--- src/c.v
+++ src/c.v
@@ -3,6 +3,14 @@ module libgo
 #flag -I@VMODROOT/go
 #flag @VMODROOT/go/libgo.a
 
+$if darwin {
+	$if arch == "arm64" {
+		#flag -arch arm64
+	} else {
+		#flag -arch amd64
+	}
+}
+
 #include "libgo.h"
 
 pub fn C.download_file(c_url &char,	c_path &char) &char
